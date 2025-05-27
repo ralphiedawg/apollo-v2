ca33617737f9ac8bd960e18713b69599d4739ba1
ralphiedawg <russellwpage@gmail.com>
2025-05-27 17:03:48 -0400

    create init script

---
+++ init.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# Initializes Apollo Env
+printf "Initializing python environment \n" 
+python3.10 -m venv venv
+source venv/bin/activate
+printf "Installing Dependencies \n" 
+python3 -m pip install -r requirements.txt
+
+printf "Initialization completed. Run the following commands to run Apollo: \n source venv/bin/activate \n python3 main.py  "
