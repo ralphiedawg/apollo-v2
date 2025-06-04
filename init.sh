f43f847c1b381b43b1664e62acc54454cbafa729
ralphiedawg <russellwpage@gmail.com>
2025-06-04 10:46:35 -0400

    update init script

--- init.sh
+++ init.sh
@@ -7,4 +7,6 @@ printf "Installing Dependencies \n"
 python3 -m pip install -r requirements.txt
 printf "Installing LLM Dependencies \n"
 ollama pull llama3.2
+printf '{ \n "facts":[ \n ] \n }' > cache/long_term_memory.json
+touch cache/output.wav
 printf "Initialization completed. Run the following commands to run Apollo: \n source venv/bin/activate \n python3 main.py  "
